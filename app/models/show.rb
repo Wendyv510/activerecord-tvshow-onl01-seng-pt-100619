21647baf3005611a7c944370be07393b2bdf7959
Wendy Harris <wendyv510@hotmail.com>
2020-01-09 01:09:52 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,34 @@
+
+class Show < ActiveRecord::Base
+  
+  attr_accessor :name, :network, :day, :rating, :season 
+  
+  def self.highest_rating 
+    self.maximum(:rating)
+  end 
+  
+  def self.most_popular_show 
+    where (':rating = ?'. self.highest_rating).first
+  end 
+  
+  def lowest_rating 
+    Show.minimum(:rating) 
+  end 
+  
+  def least_popular_show 
+    Show.lowest_rating 
+  end 
+  
+  def ratings_sum 
+    Show.sum(:rating)
+  end 
+  
+  def popular_shows 
+    Show.where(":rating > ?",5)
+  end 
+  
+  def shows_by_alphabetical_order 
+    Show.order(:name) 
+  end 
+  
+end 
